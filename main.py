46cf76a5879e00b323e5afc45fe6169f13197f20
ony-pxl <gabarda.yno.espera@gmail.com>
2026-01-17 17:37:28 +0800

    UPD: bug fixes

--- main.py
+++ main.py
@@ -55,7 +55,9 @@ class player(object):
         self.hitbox = (self.x + 17, self.y + 11, 29, 52)
         #pygame.draw.rect(win, (255,0,0), self.hitbox, 2)
     
-    def hit(self):            
+    def hit(self):       
+        self.isJump = False
+        self.jumpCount = 10
         self.x = 60
         self.y = 410
         self.walkCount = 0
@@ -145,7 +147,7 @@ class enemy(object):
 def redrawGameWindow():
     win.blit(bg, (0,0))
     text = font.render('Score: ' + str(score), 1, (0,0,0))
-    win.blit(text, (350, 10))
+    win.blit(text, (320, 10))
     man.draw(win)
     goblin.draw(win)
     for bullet in bullets:
@@ -162,10 +164,11 @@ run = True
 while run:
     clock.tick(27)
     
-    if man.hitbox[1] < goblin.hitbox[1] + goblin.hitbox[3] and man.hitbox[1] + man.hitbox[3] > goblin.hitbox[1]:
-        if man.hitbox[0] + man.hitbox[2] > goblin.hitbox[0] and man.hitbox[0] < goblin.hitbox[0] + goblin.hitbox[2]:
-            man.hit() 
-            score -= 5
+    if goblin.visible == True:
+        if man.hitbox[1] < goblin.hitbox[1] + goblin.hitbox[3] and man.hitbox[1] + man.hitbox[3] > goblin.hitbox[1]:
+            if man.hitbox[0] + man.hitbox[2] > goblin.hitbox[0] and man.hitbox[0] < goblin.hitbox[0] + goblin.hitbox[2]:
+                man.hit() 
+                score -= 5
 
     if shootLoop > 0:
         shootLoop += 1
@@ -232,6 +235,5 @@ while run:
             man.isJump = False
             man.jumpCount = 10
         
-    print(man.y)
     redrawGameWindow()
 pygame.quit()
